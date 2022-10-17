fd23fd0ebdb5b92668832abf667cd93e4c790367
Kline Gareth <kline@HUAWEI_P20_lite-3a1004243.ThinkCoffee>
2022-10-17 15:24:55 -0400

    Added chalk and chalky output :)

--- index.js
+++ index.js
@@ -3,16 +3,34 @@
 // Traceomancy
 // Uses round trip times as basis for fortune telling
 
-traceroute = require('traceroute');
+import traceroute from 'traceroute';
+import chalk from 'chalk';
 
 traceroute.trace('google.com', function (err, hops) {
 	if (! err) {
+		let values = [];
 		hops.forEach(function(hop) {
 		  Object.keys(hop).forEach(function(key) {
-			console.log(Math.floor(hop[key]));
+			values.push(Math.floor(hop[key]));
 		  });
 		});
+		withASCII(values);
 	}
 });
 
-
+const withASCII = (values) => {
+	let chalkyNums = [];
+	for (let value in values) {
+		let color = chalk.rgb(Math.floor((Math.random() * 255)), Math.floor((Math.random() * 255)), Math.floor((Math.random() * 255)));
+		chalkyNums.push(color(values[value]));
+	}
+	console.log(`
+        _      				   
+   _     /||   			  *	   		${chalkyNums[2]}  *
+  ( }    \||D  *  	${chalkyNums[0]}   *		 ${chalkyNums[4]}
+| /\__,=_[_]  		*		  	*			${chalkyNums[3]} 
+|_\_  |----|                 ${chalkyNums[1]}    	* 
+|  |/ |    |                   
+|  /_ |    |                   
+		`);
+}
